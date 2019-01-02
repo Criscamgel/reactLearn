2897b2ef7b9e80805f3e71e675e897b5c0e57a30
Criscamgel <camiloguio@gmail.com>
2019-01-02 12:01:06 -0500

    Apuntes primer compendio

---
+++ AngularJS/1/script.js
@@ -0,0 +1,18 @@
+var app = angular.module("computer",['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider){
+
+    $routeProvider.
+    when('/main', {
+        templateUrl: 'main.html',
+        controller: 'MainCtrl'
+    });
+
+}])
+
+.controller('MainCtrl', [function(){
+
+    console.log('This is the MainCtrl');
+
+}])
+
