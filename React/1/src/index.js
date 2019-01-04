0235fb0c4f3ab2d0eac82a3468e9f91ba95ddd34
Criscamgel <camiloguio@gmail.com>
2019-01-04 12:34:13 -0500

    CreateElement

--- React/1/src/index.js
+++ React/1/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 
 
 const App = () =>{
-    return React.createElement('h1',{className:'title', id:'elId'}, React.createElement('div'),'Hola Mundo')
+    /*return React.createElement('h1',{className:'title', id:'elId'}, React.createElement('div'))*/
+    return <h1>Hello react !!</h1>
 }
 
 ReactDOM.render(<App/>,document.querySelector('#root'));
