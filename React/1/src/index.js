909e20605166ee25c47a7dbac8e0d8afa7bff25f
Criscamgel <camiloguio@gmail.com>
2019-01-04 22:22:24 -0500

    Component created

--- React/1/src/index.js
+++ React/1/src/index.js
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom';
 
 
 const App = () =>{
-    /*return React.createElement('h1',{className:'title', id:'elId'}, React.createElement('div'))*/
-    return <h1>Hello react !!</h1>
+    //return React.createElement('h1',{className:'title', id:'elId'}, React.createElement('div', 'hola'))
+    //Return con un línea --->
+    //return <h1>Hello react !!</h1>
+    //Return con varias líneas
+    return (
+        <div className="newElement">
+            <h1>Hello React</h1>
+            <div>Hey</div>
+        </div>
+    )
 }
 
 ReactDOM.render(<App/>,document.querySelector('#root'));
