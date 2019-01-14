fbeb9d2e2c9e77f59eeac45f1c37a5d383644c9c
Criscamgel <camilo.guio@trycore.com.co>
2019-01-14 14:16:15 -0500

    Render Component React

--- React/1/src/index.js
+++ React/1/src/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-const App = () =>{
-    //return React.createElement('h1',{className:'title', id:'elId'}, React.createElement('div', 'hola'))
+const App = () =>{ //Arrow Function
+    return React.createElement('h1',{className:'title', id:'elId'}, 'Holaa')
+    
     //Return con un línea --->
     //return <h1>Hello react !!</h1>
-    //Return con varias líneas
-    return (
+
+    //Return con varias líneas --->
+    /*return (
         <div className="newElement">
             <h1>Hello React</h1>
             <div>Hey</div>
         </div>
-    )
+    )*/
+
+
 }
 
 ReactDOM.render(<App/>,document.querySelector('#root'));
