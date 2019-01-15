2ef8642bfe0247049f8c53a7981a5acfa128a817
Criscamgel <camilo.guio@trycore.com.co>
2019-01-15 14:43:52 -0500

    Styles in component cap 8

--- React/1/src/components/header.js
+++ React/1/src/components/header.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const getYear = () => {
+//const getYear = () => {
+    //const newDate = new Date();
+    //return newDate.getFullYear();
+//}
 
-    const newDate = new Date();
-    return newDate.getFullYear();
+class Header extends Component {
+
+    render(){
+
+        const styles = {
+
+            header: {
+                background: '#03a9f4'
+            },
+            logo:{
+                 color: '#fff',
+                 fontFamily: 'Anton',
+                 textAlign: 'center'
+            }
+
+        }
+
+        return (
+            <header style={styles.header}>
+                <div style={styles.logo}>Logo</div>
+                <input type="text"/>
+            </header>
+        )
+    }
 
 }
-
-const Header = () => {
-
-    return <div></div>
-
-    )
 
 
 export default Header;
