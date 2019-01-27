27a283a91f1a50f9ce3fa84c15e286e981c6026e
Criscamgel <camiloguio@gmail.com>
2019-01-27 13:46:16 -0500

    Integrando Estilos en los componentes

--- React/1/src/components/header.js
+++ React/1/src/components/header.js
@@ -1,30 +1,13 @@
 import React, { Component } from 'react';
-
-//const getYear = () => {
-    //const newDate = new Date();
-    //return newDate.getFullYear();
-//}
+import '../css/styles.css'
 
 class Header extends Component {
 
     render(){
 
-        const styles = {
-
-            header: {
-                background: '#03a9f4'
-            },
-            logo:{
-                 color: '#fff',
-                 fontFamily: 'Anton',
-                 textAlign: 'center'
-            }
-
-        }
-
         return (
-            <header style={styles.header}>
-                <div style={styles.logo}>Logo</div>
+            <header>
+                <div className="logo">Logo</div>
                 <input type="text"/>
             </header>
         )
