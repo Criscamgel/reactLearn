a7fac088e68bb72586edc756500c68005993ecf5
Camilo <camilo.guio@trycore.com.co>
2019-05-17 10:41:15 -0500

    Conectando Header y Router

--- React/Udemy-React/Curso10App/ProyectoCrudAxiosRouter/reactcrud/src/Componentes/Header.js
+++ React/Udemy-React/Curso10App/ProyectoCrudAxiosRouter/reactcrud/src/Componentes/Header.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import axios from 'axios';
 
 const Header = () => {
     return (
         <header className="cabecera">
             <Link to={'/'}>
-                <h1>
-                </h1>
+                <h1>Header Crud</h1>
             </Link>
         </header>
     )
--- React/Udemy-React/Curso10App/ProyectoCrudAxiosRouter/reactcrud/src/Componentes/Router.js
+++ React/Udemy-React/Curso10App/ProyectoCrudAxiosRouter/reactcrud/src/Componentes/Router.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import axios from 'axios';
+import Header from './Header';
 
 class Router extends Component {
     state = {
@@ -8,7 +10,9 @@ class Router extends Component {
     render(){
         return(
 
-            
+        <BrowserRouter>
+            <Header/>
+        </BrowserRouter>    
 
         );
     }
